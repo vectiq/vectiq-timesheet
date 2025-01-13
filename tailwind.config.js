cd38cdb787ec0161ff196c4a3cdfd51acbff95f9
Dan <1719975+dbnardi@users.noreply.github.com>
2025-01-14 07:59:21 +1100

    add our fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,23 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Aeonik', 'system-ui', 'sans-serif']
+      },
+      letterSpacing: {
+        'tighter': '-0.05em',
+        'tight': '-0.025em', 
+        'normal': '0em',
+        'wide': '0.025em',
+        'wider': '0.05em',
+        'widest': '0.1em'
+      },
+      lineHeight: {
+        'tighter': '1.15',
+        'snug': '1.25',
+        'relaxed': '1.625',
+        'loose': '2'
+      },
       colors: {
         surface: {
           50: '#fafafa',
