9772620502da1a9b6b322157dcad2bbfdc272532
Dan <1719975+dbnardi@users.noreply.github.com>
2025-01-13 15:10:02 +1100

    logo back

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -28,11 +28,11 @@ export function Navbar() {
       <div className="px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between">
           <div className="flex items-center gap-4">
-            {/* <img
+            <img
               className="hidden md:block h-10 w-auto"
               src="/logo.svg"
               alt="Company Logo"
-            /> */}
+            />
             <MobileNav />
           </div>
           
